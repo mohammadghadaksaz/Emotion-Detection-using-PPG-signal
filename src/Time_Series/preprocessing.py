1534a0e4e5122846b732b5abcd60d4daecdb1610
Mohammadmahdi Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-10-27 14:37:30 -0400

    transition_mask function is written to create windows

--- src/Time_Series/preprocessing.py
+++ src/Time_Series/preprocessing.py
@@ -5,3 +5,11 @@ def butter_bandpass(x, fs, lo, hi, order=4):
     sos = butter(order, [lo/(fs/2), hi/(fs/2)], btype="band", output="sos")
     return sosfiltfilt(sos, x)
 
+def build_transition_mask(labels, fs_lbl, margin_s=30):
+    change_idx = np.where(np.diff(labels) != 0)[0]
+    mask = np.zeros_like(labels, dtype=bool)
+    m = int(margin_s * fs_lbl)
+    for i in change_idx:
+        a, b = max(0, i - m), min(len(labels), i + m)
+        mask[a:b] = True
+    return mask
